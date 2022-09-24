ff4ec608f1284aa7e208fb05f4bd71e41335e386
Merlini <merlini.jayson@gmail.com>
2022-09-24 14:12:53 +0200

    try to get position of each frame

--- src/elements/game.ts
+++ src/elements/game.ts
@@ -1,4 +1,4 @@
-import Player from './player'
+import Player from './player';
 
 export default class Game implements ISystem {
   player: Player
@@ -13,24 +13,10 @@ export default class Game implements ISystem {
   }
 
   update() {
-    
-    const angle = Vector3.GetAngleBetweenVectors(new Vector3(0, 0, 0), Camera.instance.feetPosition, new Vector3(0, 0, 0));
-    log(angle);
-    /*if (
-      this.prevPosition.x !== Camera.instance.feetPosition.x ||
-      this.prevPosition.z !== Camera.instance.feetPosition.z
-    ) {*/
-      /*log('------')
-      log(Camera.instance.feetPosition)
-      log(this.prevPosition)
-      log('------')
-      const x = Camera.instance.feetPosition.x - this.prevPosition.x
-      const y = Camera.instance.feetPosition.z - this.prevPosition.z
-      log('x =', x)
-      log('y =', y)
-      this.angle = Math.atan2(x, y)
-      log(this.angle)
+    if (this.player.position != Camera.instance.feetPosition) {
+      this.player.updatePosition(Camera.instance.feetPosition);
+      log(this.player.position)
     }
-    this.prevPosition = Camera.instance.feetPosition*/
-  //}
+    //log(this.player.position)
+  }
 }
--- src/elements/player.ts
+++ src/elements/player.ts
@@ -1,11 +1,17 @@
 export default class Player extends Entity {
+  position: Vector3
   constructor() {
     super()
     this.addComponent(
       new Transform({
         position: new Vector3(0, 0, 0)
       })
-    )
-    engine.addEntity(this)
-  }
+      )
+      this.position = new Vector3(0,0,0)
+      engine.addEntity(this)
+    }
+
+    updatePosition(newPosition: Vector3) {
+      this.position = newPosition
+    }
 }
